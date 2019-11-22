c3f59d96e39c6e1fad4c15808ed5ced09957e52b
Madeline Richards <43390250+madejoyr@users.noreply.github.com>
2019-11-22 17:39:15 -0500

    Madeline's Changes

--- exercise_1.py
+++ exercise_1.py
@@ -1,8 +1,8 @@
-MEMBER_1 = "Tiffany"
+MEMBER_1 = "Madeline"
 MEMBER_2 = "Allen"
 MEMBER_3 = "Ramon"
 
-MEMBER_1_HOME = "Richmond, VA"
+MEMBER_1_HOME = "St. Joseph, MI"
 MEMBER_2_HOME = "Haiphong, Viet Nam"
 MEMBER_3_HOME = "Monterrey, Mexico"
 
